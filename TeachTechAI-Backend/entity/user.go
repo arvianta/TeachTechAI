1a5dfaaa5d25d7fadd0f4bbe5388d24460d6196d
Rayhan Arvianta Bayuputra <rayhan.arvianta@gmail.com>
2024-05-14 09:47:05 +0700

    feat: user entity

--- TeachTechAI-Backend/entity/user.go
+++ TeachTechAI-Backend/entity/user.go
@@ -1,1 +1,26 @@
 package entity
+
+import (
+	"teach-tech-ai/helpers"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        	uuid.UUID   `gorm:"primary_key;not_null" json:"id"`
+	Name 		string 		`json:"name"`
+	Email 		string 		`json:"email" binding:"email"`
+	Phone 		string 		`json:"phone"`
+	Password 	string  	`json:"password"`
+	Role		string		`json:"role`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) error {
+	var err error
+	u.Password, err = helpers.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- TeachTechAI-Backend/go.mod
+++ TeachTechAI-Backend/go.mod
@@ -1,3 +1,14 @@
-module teach-teach-ai.com
+module teach-tech-ai
 
 go 1.22.3
+
+require (
+	github.com/google/uuid v1.6.0
+	golang.org/x/crypto v0.23.0
+	gorm.io/gorm v1.25.10
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+)
