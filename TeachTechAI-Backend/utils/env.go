598f7bcfcd43ad712c92cb034edbaba44cb19b32
9ninelie <kenkazuki99@gmail.com>
2024-06-08 18:35:49 +0700

    feat: update env.go for gcr

--- TeachTechAI-Backend/utils/env.go
+++ TeachTechAI-Backend/utils/env.go
@@ -8,6 +8,15 @@ import (
 )
 
 func MustGetenv(k string) string {
+
+	if os.Getenv("APP_ENV") == "production" {
+		v := os.Getenv(k)
+		if v == "" {
+			log.Fatalf("%s environment variable not set.", k)
+		}
+		return v
+	}
+
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
